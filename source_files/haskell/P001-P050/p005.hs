2702678dafc962b2e4ae90400a19e514c8ea31c2
Ben Fidlers <ben.fidlers@student.kuleuven.be>
2021-03-21 17:16:11 +0100

    removed unnecessary lambda function

--- source_files/haskell/P001-P050/p005.hs
+++ source_files/haskell/P001-P050/p005.hs
@@ -1,3 +1,3 @@
 -- What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20? 
 
-main = print . foldr (\a b -> lcm a b) 1 $ [1..20]
+main = print . foldr lcm 1 $ [1..20]
