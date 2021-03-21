344342fc51d1500aac628873eef57031c3f53f0a
Ben Fidlers <ben.fidlers@student.kuleuven.be>
2021-03-22 00:57:43 +0100

    introduced digitToInt function

--- source_files/haskell/P001-P050/p016.hs
+++ source_files/haskell/P001-P050/p016.hs
@@ -1,7 +1,9 @@
 {-
 215 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
 
-What is the sum of the digits of the number 21000?
+What is the sum of the digits of the number 2^1000?
 -}
 
-main = print . sum . map (\ x -> read [x] :: Int) . show $ 2^1000
+import Data.Char
+
+main = print . sum . map (digitToInt) . show $ 2^1000
