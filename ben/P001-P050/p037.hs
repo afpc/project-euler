50d9d7219ec9f72dd14d539f547aecc4adf4f5c7
Ben Fidlers <ben.fidlers@gmail.com>
2021-04-07 22:33:26 +0200

    solution p037 haskell

---
+++ ben/P001-P050/p037.hs
@@ -0,0 +1,42 @@
+{-
+The number 3797 has an interesting property. Being prime itself, it is possible to continuously remove digits from left to right, and remain prime at each stage: 3797, 797, 97, and 7. Similarly we can work from right to left: 3797, 379, 37, and 3.
+
+Find the sum of the only eleven primes that are both truncatable from left to right and right to left.
+
+NOTE: 2, 3, 5, and 7 are not considered to be truncatable primes.
+-}
+
+import Data.List
+
+-- Kind of inefficient.
+main = print . sum . take 11 $ getNumbers
+
+getNumbers :: [Int]
+getNumbers = [nb | nb <- primes,
+                   nb > 10,
+                   let s = show nb,
+                   not (elem '4' s),
+                   not (elem '6' s),
+                   not (elem '8' s),
+                   leftTruncatable s,
+                   rightTruncatable s]
+
+leftTruncatable :: String -> Bool
+leftTruncatable [] = True
+leftTruncatable (x:xs) = leftTruncatable xs && elem (read (x:xs)) primes 
+                   
+rightTruncatable :: String -> Bool
+rightTruncatable [] = True
+rightTruncatable nb = rightTruncatable (init nb) && elem (read nb) primes 
+
+primes :: [Int]
+primes = sieve 1000000 [2..1000000]
+
+sieve :: Int -> [Int] -> [Int]
+sieve _ [] = []
+sieve limit (x:xs)
+    | fromIntegral x < sqrt (fromIntegral limit) = x : sieve limit (removeMultiples (x:xs))
+    | otherwise = (x:xs)
+
+removeMultiples :: [Int] -> [Int]
+removeMultiples list = filter (\ a -> mod a (head list) /= 0) list
